cc390b93377426ed5119ecd25fd95c389b2f65ea
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-10-15 14:46:33 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Amahastla on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
